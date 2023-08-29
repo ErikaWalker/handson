a8af0d86e0661fe17bb64ae7054a9943af6a43d8
Aluno <aluno@edu.senac.br>
2023-08-29 19:45:45 -0300

    testando nuvem

---
+++ JS/handson02.js
@@ -0,0 +1,5 @@
+function NomeCompleto(){
+    var nome = document.getElementById("nome").value;
+    var sobrenome = document.getElementById("sobrenome").value;
+    alert(`O seu nome completo ${nome} ${sobrenome}!`)
+}
---
+++ JS/handson03.js
@@ -0,0 +1,9 @@
+function somar(){
+/*Entrada*/
+var num1 = parseInt(document.ElementById("num1").value;
+var num2 = parseInt(document.ElementById("num2").value;
+/*processamento*/
+var somar = num1+num2;
+/*Saída*/
+alert(num1+num2)
+} 
